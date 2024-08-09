f0554645cae83d3d04440f1371c8fb5c9d34a3d0
Moskal Mykhailo <m.moskal2005@gmail.com>
2024-08-09 18:14:15 +0300

    [FEATURE] Add connection to MySQL

---
+++ backend/app/configs/mysql.js
@@ -0,0 +1,9 @@
+const config = {
+    MYSQL_HOST: process.env.MYSQL_HOST,
+    MYSQL_PORT: process.env.MYSQL_PORT,
+    MYSQL_DB_NAME: process.env.MYSQL_DB_NAME,
+    MYSQL_USERNAME: process.env.MYSQL_USERNAME,
+    MYSQL_PASSWORD: process.env.MYSQL_PASSWORD
+};
+
+module.exports = config;
