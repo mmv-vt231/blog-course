83aecc8d76ad3c05a900703c0a23511f8ff30c2e
Moskal Mykhailo <m.moskal2005@gmail.com>
2024-08-16 20:23:15 +0300

    [FEATURE] Add user endpoints

--- backend/app/routers/index.js
+++ backend/app/routers/index.js
@@ -1,11 +1,15 @@
 const post = require("../src/post/routes");
 const tag = require("../src/tag/routes");
 const like = require("../src/like/routes");
+const role = require("../src/role/routes");
+const user = require("../src/user/routes");
 
 module.exports = (app) => {
     app.use("/post", post);
     app.use("/tag", tag);
     app.use("/like", like);
+    app.use("/role", role);
+    app.use("/user", user);
 
     app.use("*", (req, res) => {
         res.status(404).send("Not Found");
