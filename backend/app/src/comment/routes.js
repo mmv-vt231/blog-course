16527fe573870033d65f27fc1fe23f20d7eb6e77
Moskal Mykhailo <m.moskal2005@gmail.com>
2024-08-26 18:56:26 +0300

    [FIX] Tag and comment passport missing require

--- backend/app/src/comment/routes.js
+++ backend/app/src/comment/routes.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const passport = require("passport");
+
 const controller = require('./controller/index');
 
 const router = express.Router();
--- backend/app/src/user/controller/index.js
+++ backend/app/src/user/controller/index.js
@@ -1,7 +1,6 @@
 const User = require("./../../../models/User");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const Post = require("../../../models/Post");
 
 module.exports.login = async (req, res) => {
     try {
